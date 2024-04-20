1d78d16ad96214cb959b1d7648ad82bdab79adb2
gauravjagwani <gauravjagwani8@gmail.com>
2024-04-20 12:55:30 +0530

    new logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/CINEFLIX.png";
+import logo from "../../assets/Cineflix-logo.png";
 
 const Header = () => {
   const [show, setShow] = useState("top");
